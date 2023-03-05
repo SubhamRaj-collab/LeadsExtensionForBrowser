b7f0f44316749e8784bef5bce4c70b8fd347d2a1
Subham Raj <glorious10info@gmail.com>
2023-03-05 19:49:10 +0530

    Completed Chrome Extension app with add input, add tab and delete all functions

--- script.js
+++ script.js
@@ -1,48 +1,75 @@
 const ulEl = document.getElementById('ul-el')
 const inputBtn = document.getElementById('input-btn')
+const deleteBtn = document.getElementById('delete-btn')
 const inputEl = document.getElementById('input-el')
-let myLeads = []
+const savetabBtn = document.getElementById('saveTab-btn')
+let myLeads = localStorage.getItem('myLeads')
+
+if(myLeads == null)
+{
+    myLeads = `[]`
+}
+
+let leads = JSON.parse(localStorage.getItem("myLeads"))
+render(leads)
+
+savetabBtn.addEventListener('click', function(){
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
+        console.log(tabs)
+        
+        let leads = JSON.parse(localStorage.getItem("myLeads"))
+        if(leads == null)
+        {
+            leads = []
+        }
+        leads.push(tabs[0].url)
+        render(leads)
+        leads = JSON.stringify(leads)
+        localStorage.setItem("myLeads", leads)
+    })
+
+})
+
+deleteBtn.addEventListener('dblclick', function(){
+
+    localStorage.clear()
+    myLeads = '[]'
+    render(JSON.parse(myLeads))
+
+})
 
 inputBtn.addEventListener('click', function(){
 
     if(inputEl.value != '')
     {
+        myLeads = JSON.parse(localStorage.getItem("myLeads"))
         myLeads.push(inputEl.value)
         inputEl.value = ''
-        
+        render(myLeads)
+        myLeads = JSON.stringify(myLeads)
+        localStorage.setItem("myLeads",myLeads)
     }   
 
     console.log(myLeads)
-    renderLeads()
+    
+
 })
 
-function renderLeads()
+function render(leads)
 {
     let listItems = ""
 
-    for(let i in myLeads)
+    for(let i in leads)
     {
-        // listItems += '<li>'+"<a href = "+ myLeads[i] +" target='_blank'>"+ myLeads[i] +'</a>'+'</li>'
-        // Normal Statement with HTML tags
 
         listItems += `<li>
-            <a href = '${myLeads[i]}' target='_blank'>
-                ${myLeads[i]} 
+            <a href = '${leads[i]}' target='_blank'>
+                ${leads[i]} 
             </a>
         </li>`
-        // Template String/literal Method of writing statements with HTML tags.
-
-        // Method1
-        // Create Element
-        // set textContent
-        // append to ul
-        // const li = document.createElement('li')
-        // li.textContent = myLeads[i]
-        // ulEl.append(li)
-        // Method2
     }
 
     ulEl.innerHTML = listItems
-    // DOM manipulation comes with a cost, so it must be done least number of times
 }
 
